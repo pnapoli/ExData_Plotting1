ed44d14ef3c7377b4abe2c7ab02c3cb28b53e7b1
pnapoli <napoli.p@gmail.com>
2016-04-03 22:51:23 -0300

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,9 +1,14 @@
-
-dataFile <- "./data/household_power_consumption.txt"
+# ----------------------------------------------------------
+# Getting the full dataset
+# ----------------------------------------------------------
+dataFile <- "./ExpDataAnalysis/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
+
+# ----------------------------------------------------------
+# Subsetting the data
+# ----------------------------------------------------------
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
@@ -12,8 +17,9 @@ subMetering1 <- as.numeric(subSetData$Sub_metering_1)
 subMetering2 <- as.numeric(subSetData$Sub_metering_2)
 subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
-
-png("plot4.png", width=480, height=480)
+# ----------------------------------------------------------
+# Plotting
+# ----------------------------------------------------------
 par(mfrow = c(2, 2)) 
 
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
@@ -27,4 +33,8 @@ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 
+# ----------------------------------------------------------
+# Saving to PNG file
+# ----------------------------------------------------------
+dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
