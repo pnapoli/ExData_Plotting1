5af7bfee2cb4982d842cb0064c8e22f22fb9d7b3
pnapoli <napoli.p@gmail.com>
2016-04-03 21:34:37 -0300

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -8,11 +8,14 @@ data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="
 # Subsetting the data
 # ----------------------------------------------------------
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subSetData$Global_active_power)
 
 # ----------------------------------------------------------
 # Plot 1
 # ----------------------------------------------------------
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(globalActivePower, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
